1523db59274a904f4b5b8dad034fa6f7f282e573
zynfly <fly@zynfly.wang>
2019-10-11 11:42:53 +0800

    Support traditional Wordpress theme and modify routing load timing

---
+++ src/Route/Hooks/WpTheme.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Themosis\Route\Hooks;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+use Themosis\Hook\Hookable;
+use Themosis\Support\Facades\Action;
+use App\Http\Kernel;
+use Themosis\Core\Application;
+use Themosis\Support\Facades\Filter;
+
+class WpTheme extends Hookable
+{
+
+    /**
+     * @var $request Request
+     */
+    protected $request;
+    /**
+     * @var $response Response
+     */
+    protected $response;
+    /**
+     * @var $kernel Kernel
+     */
+    protected $kernel;
+
+    public function register()
+    {
+        Action::add('template_redirect', [$this, 'template_redirect'], PHP_INT_MAX);
+        Filter::add('template_include', [$this, 'template_include'], PHP_INT_MAX);
+    }
+
+
+    protected function response_send()
+    {
+        $this->response->send();
+        $this->kernel->terminate($request, $this->response);
+        exit;
+    }
+
+    public
+    function template_redirect()
+    {
+        $app = Application::getInstance();
+        $this->kernel = $app->make(Kernel::class);
+
+        $this->request = $app['request'];
+        $this->response = $this->kernel->handle($this->request);
+
+
+        if (404 != $this->response->getStatusCode() && 'is_404' != $this->request->route()->getCondition()) {
+            $this->response_send();
+        }
+    }
+
+    public function template_include($template)
+    {
+        if (Str::endsWith($template, '404.php') && !empty($this->request->route())) {
+            $this->response_send();
+        }
+        error_reporting(0);
+        return $template;
+    }
+}
